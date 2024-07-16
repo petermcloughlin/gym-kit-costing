7b099693647a6975abe404364983209bdbe02580
Peter Mcloughlin <mcloughlin78@gmail.com>
2024-07-16 10:35:09 +0000

    Add pseudo code to layout function logic

--- run.py
+++ run.py
@@ -2,3 +2,18 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 print("Welcome to Pete's Gym Kit Costing")
+
+# Function to get user height in meters
+
+# Function to get user weight in kilograms
+
+# Function to calculate users BMI
+
+# Function to get users age
+
+# Function to get users workout schedule (num of days per week)
+
+# Function to validate user height and weight input
+
+# Function to get array values of user data to recommend gym dumbell set and cost  
+
