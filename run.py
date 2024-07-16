e9cbc5e172a30dc49c9b6c4fc97631581bbcf3e9
Peter Mcloughlin <mcloughlin78@gmail.com>
2024-07-16 10:38:01 +0000

    Add more pseudo code

--- run.py
+++ run.py
@@ -1,5 +1,3 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 print("Welcome to Pete's Gym Kit Costing")
 
@@ -17,3 +15,4 @@ print("Welcome to Pete's Gym Kit Costing")
 
 # Function to get array values of user data to recommend gym dumbell set and cost  
 
+# Function to check user age, bmi to return a dumbell set and cost
