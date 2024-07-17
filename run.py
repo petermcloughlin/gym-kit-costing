d08ed85af10b2d6e158dd3f239505bb6e603f9a4
Peter Mcloughlin <mcloughlin78@gmail.com>
2024-07-17 11:20:47 +0000

    Add exception handling to functions and new function to request gender

--- run.py
+++ run.py
@@ -1,13 +1,14 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 CUSTOMER = []
+
 # Function to get user height in meters
-def get_user_height():   
-    '''
-    Get user height in cms 
-    ''' 
+
+
+def get_user_height():
+    '''Get user height in cms'''
     while True:
         try:
-            height = int(input("Please enter your height in centimeters. Type a number:\n"))
+            height = int(input("Enter your height in cms.Type a number:\n"))
         except ValueError:
             print("Sorry, you must type a number for your height.")
             continue
@@ -16,58 +17,75 @@ def get_user_height():
             break
 
 # Function to get user weight in kilograms
-def get_user_weight():
-    ''' 
-    Get user weight in cms 
-    '''
-    weight = input(f'Please enter your weight in kilograms:\n')
-    CUSTOMER.append(weight)  
-    
-# Function to calculate users BMI
-def get_user_bmi(height, weight):
-    ''' 
-    Calculate the user's BMI
-    '''
-    bmi = round((int(weight) / int(height) / int(height)) * 10000 , 1)
-    CUSTOMER.append(bmi)    
 
+
+def get_user_weight():
+    '''Get user weight in kgs'''
+    while True:
+        try:
+            weight = int(input(f'Please enter your weight in kilograms:\n'))
+        except ValueError:
+            print("Sorry, you must type a number for your weight.")
+            continue
+        else:
+            CUSTOMER.append(weight)
+            break    
+# Function to calculate users BMI
+
+
+def get_user_bmi(height, weight):
+    '''Calculate the user's BMI'''
+    bmi = round((int(weight) / int(height) / int(height)) * 10000, 1)
+    CUSTOMER.append(bmi)
 # Function to get users age
+
+
 def get_user_age():
-    ''' 
-    Get user age
-    '''
-    age = input(f'Please enter your age in years (enter a number only):\n')
-    CUSTOMER.append(age)    
+    '''Get user age'''
+    while True:
+        try:
+            age = int(input(f'Please enter your age in years (enter a number only):\n'))
+        except ValueError:
+            print("Sorry, you must type a number for your age.")
+            continue
+        else:
+            CUSTOMER.append(age)
+            break   
+# Function to get user's gender
+
+
+def get_user_gender():
+    '''Get user gender'''
+    while True:
+        try:
+            gender = input(f'Please enter your gender (m for male, f for female):\n')
+        except ValueError:
+            print("Sorry, you must type 'm' or 'f' for your gender.")
+            continue
+        else:
+            CUSTOMER.append(gender)
+            break 
 # Function to get users workout schedule (num of days per week)
 
-# Function to validate user height and weight input
-def validate_input(user_input):
-    try:        
-        if type(user_input) != int:
-            raise ValueError(
-                f"Please enter a valid number. You typed {user_input}"
-                )
-                 
-    except ValueError as e:
-        print(f'Invalid data {e}, please try again.')
-        return False
-
-    return True
-
-# Function to get array values of user data to recommend gym dumbell set and cost  
+# Function to get array values of user data to recommend kit and cost
 
 # Function to check user age, bmi to return a dumbell set and cost
 
 # main method call
-def main():    
+
+
+def main():
     # call all functions
-    get_user_height()   
-    get_user_weight()
+    get_user_height()
+    get_user_weight()    
     get_user_bmi(CUSTOMER[0], CUSTOMER[1])
-    get_user_age()    
-    print(f'Height: {CUSTOMER[0]}\nWeight: {CUSTOMER[1]}\nBMI: {CUSTOMER[2]}\nAge: {CUSTOMER[3]}\n')
+    get_user_age()
+    get_user_gender()
+    print('---------- Customer Information ----------')
+    print(f'Height: {CUSTOMER[0]}\nWeight: {CUSTOMER[1]}\nBMI: {CUSTOMER[2]}\nAge: {CUSTOMER[3]}\nGender: {CUSTOMER[4]}')
 print("---------------------------------------------")
 print("----  Welcome to Pete's Gym Kit Costing  ----")
 print("---------------------------------------------")
 print('')
+print('Just a few questions before recommending a gym kit and cost.')
 main()
